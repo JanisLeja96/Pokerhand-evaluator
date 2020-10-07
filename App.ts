8ff588f1b38c10ad4b3ac730f5b835a57f7aa546
JanisLeja1996 <janis.leja1996@gmail.com>
2020-10-07 14:58:47 +0300

    Removed unneeded imports

--- App.ts
+++ App.ts
@@ -1,9 +1,4 @@
-import { Card } from './src/GameElements/Card';
-import { Board } from './src/GameElements/Board';
-import { PlayerHand } from './src/GameElements/PlayerHand';
 import { Game } from './src/Components/Game';
-import { InputParser } from './src/Components/InputParser';
-import { Evaluator } from './src/Components/Evaluator';
 import readline from 'readline';
 
 const input = readline.createInterface({
